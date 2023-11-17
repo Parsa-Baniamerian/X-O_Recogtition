7e72b3d4b9a66cc98e13437036bbe941d795ce9a
ParsaBaniamerian <parsabaniamerian03@gmail.com>
2023-11-18 01:08:09 +0330

    training features added to GUI

--- x-o projects/GUI.py
+++ x-o projects/GUI.py
@@ -12,13 +12,15 @@ dark_hover_color = "#1e3e63"
 
 def prepare_data():
     new_data = []
-    input_label = switch.get()  # 0 for label O and 1 for label X
+    input_label = switch.get()
+    if input_label == 0:
+        input_label = -1
     new_data.append(input_label)
     for btn in buttons:
         btn_color = btn.cget("fg_color")
 
         if btn_color == dark_color:
-            new_data.append(0)
+            new_data.append(-1)
         elif btn_color == light_color:
             new_data.append(1)
     return new_data
@@ -43,21 +45,31 @@ def save_data_in_dataset(new_data):
         return True
     return False
 
+
 # * HANDLING BUTTON CLICKS
-
-
 def add_btn_clicked():
+    add_data_result_lbl.configure(text="")
     output_lbl.configure(text="")
     is_saved = save_data_in_dataset(prepare_data())
     reset_grid()
     if is_saved:
-        message_lbl.configure(text="The new data was saved in the dataset")
+        add_data_result_lbl.configure(
+            text="The new data was saved in the dataset")
     else:
-        message_lbl.configure(text="This data already exists in the dataset")
+        add_data_result_lbl.configure(
+            text="This data already exists in the dataset")
 
+
+def train_btn_clicked():
+    algorithm = algorithms_combo.get()
+    train_result_lbl.configure(text="")
+    output_lbl.configure(text="")
+    train_result_lbl.configure(text = algorithm +" algorithm was trained successfully")
 
 def test_btn_clicked():
-    message_lbl.configure(text="")
+
+    add_data_result_lbl.configure(text="")
+    train_result_lbl.configure(text="")
     output_lbl.configure(text="X")
 
 
@@ -116,6 +128,17 @@ add_btn = CTkButton(
 )
 add_btn.place(relx=0.75, rely=0.45, anchor="center")
 
+train_btn = CTkButton(
+    master=app,
+    text="Train",
+    corner_radius=7,
+    width=80,
+    height=30,
+    border_width=2,
+    command=train_btn_clicked
+)
+train_btn.place(relx=0.75, rely=0.68, anchor="center")
+
 test_btn = CTkButton(
     master=app,
     text="Test",
@@ -123,9 +146,9 @@ test_btn = CTkButton(
     width=80,
     height=30,
     border_width=2,
-    command=test_btn_clicked,
+    command=test_btn_clicked
 )
-test_btn.place(relx=0.75, rely=0.68, anchor="center")
+test_btn.place(relx=0.75, rely=0.9, anchor="center")
 
 reset_btn = CTkButton(
     master=app,
@@ -135,7 +158,7 @@ reset_btn = CTkButton(
     width=50,
     height=25,
     border_width=2,
-    command=reset_grid,
+    command=reset_grid
 )
 reset_btn.place(relx=0.11, rely=0.92, anchor="center")
 
@@ -149,6 +172,20 @@ switch = CTkSwitch(
     text_color=light_color
 )
 switch.place(relx=0.59, rely=0.45, anchor="center")
+
+
+# *COMBOBOX
+algorithms_combo = CTkComboBox(
+    master=app,
+    values=["Hebb", "Perceptron", "MLP", "Adaline"],
+    text_color=light_color,
+    font=("Atrial", 12, "bold"),
+    width=105,
+    border_color=dark_color,
+    button_color=dark_color,
+    button_hover_color=dark_hover_color
+)
+algorithms_combo.place(relx=0.58, rely=0.68, anchor="center")
 
 
 # * LABELS
@@ -170,30 +207,42 @@ title_lbl = CTkLabel(
     text="X/O Recognition",
     font=("Atrial", 32, "bold"),
     fg_color="transparent",
-    text_color=light_color,
+    text_color=light_color
 )
 title_lbl.place(relx=0.75, rely=0.15, anchor="center")
 
-message_lbl = CTkLabel(
+add_data_result_lbl = CTkLabel(
     master=app,
     width=300,
     height=10,
     text="",
     font=("Atrial", 13),
     fg_color="transparent",
-    text_color=light_color,
+    text_color=light_color
 )
-message_lbl.place(relx=0.75, rely=0.52, anchor="center")
+add_data_result_lbl.place(relx=0.75, rely=0.53, anchor="center")
+
+train_result_lbl = CTkLabel(
+    master=app,
+    width=300,
+    height=10,
+    text="",
+    font=("Atrial", 13),
+    fg_color="transparent",
+    text_color=light_color
+)
+train_result_lbl.place(relx=0.75, rely=0.76, anchor="center")
+
 
 output_lbl = CTkLabel(
     master=app,
-    width=40,
-    height=15,
+    width=60,
+    height=25,
     text="",
-    font=("Atrial", 20, "bold"),
+    font=("Atrial", 28, "bold"),
     fg_color="transparent",
-    text_color=light_color,
+    text_color=light_color
 )
-output_lbl.place(relx=0.87, rely=0.68, anchor="center")
+output_lbl.place(relx=0.87, rely=0.9, anchor="center")
 
 app.mainloop()
