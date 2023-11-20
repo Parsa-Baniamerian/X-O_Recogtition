f712ba8df5d8cdb8b3b8d619b5757960e3bfac56
ParsaBaniamerian <parsabaniamerian03@gmail.com>
2023-11-20 22:19:27 +0330

    Perceptron final version

--- x-o projects/perceptron.py
+++ x-o projects/perceptron.py
@@ -6,12 +6,13 @@ def training(dataset_file):
     weight_changed = True
     while weight_changed:
         weight_changed = False
-        with open(dataset_file, "r") as file:
-            for line in file:
+        with open(dataset_file, "r") as dataset:
+            for line in dataset:
                 training_vec = list(map(int, line.strip().split(",")))
                 target = training_vec[0]
+                yni = bias
                 for i in range(1, len(training_vec)):
-                    yni = bias + weights[i-1] * training_vec[i]
+                    yni += weights[i-1] * training_vec[i]
                 if yni > theta:
                     yni = 1
                 elif yni < -theta:
@@ -20,18 +21,19 @@ def training(dataset_file):
                     yni = 0
 
                 if yni != target:
+                    weight_changed = True
+                    bias += learning_rate * target
                     for i in range(1, len(training_vec)):
                         weights[i - 1] += learning_rate*training_vec[i]*target
-                    bias += learning_rate * target
-                    weight_changed = True
-                print(weights, bias)
+
     return weights, bias
 
 
 def testing(input_data, weights, bias):
     theta = 0.2
+    yni = bias
     for i in range(1, len(input_data)):
-        yni = bias + weights[i-1] * input_data[i]
+        yni += weights[i-1] * input_data[i]
     if yni > theta:
         return "X"
     elif yni < theta:
